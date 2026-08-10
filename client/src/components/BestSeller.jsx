5d358863adf5ee4f4ddc82f70e768fdcea9fbb8d
Deepak Chaudhary <154805773+programingwithdeepak@users.noreply.github.com>
2026-08-10 18:19:33 +0530

    Adjust grid layout for BestSeller component

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -6,7 +6,7 @@ const BestSeller = () => {
   return (
     <div className="mt-16">
       <p className="text-2xl md:text-3xl font-medium">Best Sellers</p>
-      <div className="my-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 items-center justify-center">
+      <div className="my-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
         {products
           .filter((product) => product.inStock)
           .slice(0, 5)
